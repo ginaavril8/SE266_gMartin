8d07dcfae905e56f616d0204343c93376d4e4303
Gina Martin <gamartin@email.neit.edu>
2020-12-02 08:14:30 -0500

    SQL Tables

--- W7/SQLtablecode.php
+++ W7/SQLtablecode.php
@@ -1,9 +1,24 @@
+<?php
+
+
+error_reporting(E_ALL ^ E_WARNING);
+
+
+    $images = filter_input ( INPUT_GET , 'image' );
+    if (isset($images) && !empty($images)) {
+        echo '<img src="images/'.$images.'.png">';
+    }
+    
+    
+     
+?>
+
 
 <!doctype html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Main Course</title>
+<title>Final | SQL Tables</title>
 <!-- Link to external Stylesheet -->
 <link rel="stylesheet" type="text/css" href="style.css">
 
@@ -14,7 +29,7 @@
 
 <!-- Header Div -->      
 <div class="header"> 
-<h2>PHP & MySQL</h2>
+<h2>SQL Tables</h2>
 </div><!-- End Header Div -->
 
 
@@ -31,22 +46,16 @@
 
 
 
-<!-- Container Div -->
-<div class="container">
-<ul>
-<li><a href="W1/index.php">Week 1</a></li>
-<li><a href="W2/index.php">Week 2</a></li>
-<li><a href="W3/atm.php">Week 3</a></li>
-<li><a href="W4\clients/view.php">Week 4</a></li>
-<li><a href="W5/clients/view.php">Week 5</a></li>
-<li><a href="W6/login.php">Week 6</a></li>
-<li><a href="W7/FPP.php">Week 7: Proposal</a></li>
-<li><a href="W7/Wireframes.php">Week 7: Wireframes</a></li>
-<li><a href="W7/SQLtablecode.php">Week 7: SQL Tables</a></li>
-<li><a href="W8/index.php">Week 8</a></li>
-<li><a href="W9/index.php">Week 9</a></li>
-<li><a href="W10/index.php">Week 10</a></li>
-</ul>
+ <!-- Container Div -->
+ <div class="container" style="text-align:center;">  
+
+<h3>Customers & Customer Order History</h3>
+
+               
+                <img src="images/SQLtables<?php echo $images; ?>.png" style="height:450px;"><br>
+            
+
+
 </div> <!-- End Container Div -->
 
 
